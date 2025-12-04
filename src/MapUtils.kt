5aec13f53e4de5eb85127f2f552efca874c2cc91
Tim Teichmann <TimTeichmann@posteo.net>
2025-12-04 07:06:08 +0100

    Add solution for Day04 part 1.

--- src/MapUtils.kt
+++ src/MapUtils.kt
@@ -55,18 +55,12 @@ sealed class Direction(val nextCoordinate: (IntCoordinate) -> IntCoordinate) {
 
         private val oppositeDirs by lazy {
             mapOf(
-                LR to RL,
-                RL to LR,
-                UD to DU,
-                DU to UD
+                LR to RL, RL to LR, UD to DU, DU to UD
             )
         }
         private val perpendicularDirsLists by lazy {
             mapOf(
-                LR to listOf(UD, DU),
-                RL to listOf(UD, DU),
-                UD to listOf(LR, RL),
-                DU to listOf(LR, RL)
+                LR to listOf(UD, DU), RL to listOf(UD, DU), UD to listOf(LR, RL), DU to listOf(LR, RL)
             )
         }
         private val allEntries by lazy { listOf(LR, RL, UD, DU) }
@@ -100,26 +94,18 @@ open class DataMap<T>(val data: List<List<T>>) {
 
     operator fun get(key: IntCoordinate) = data[key.first][key.second]
 
-    fun copyDataWithModification(coordinate: IntCoordinate, newValue: T) = data
-        .withIndex()
-        .map { (iRow, row) ->
-            row
-                .withIndex()
+    fun copyDataWithModification(coordinate: IntCoordinate, newValue: T) = data.withIndex().map { (iRow, row) ->
+            row.withIndex()
                 .map { if (iRow == coordinate.first && it.index == coordinate.second) newValue else it.value }
         }
 
     fun getCopyWithModifications(coordinatesValues: Map<IntCoordinate, T>) = DataMap(
-        data
-            .withIndex()
-            .map { (iRow, row) ->
-                row
-                    .withIndex()
-                    .map {
+        data.withIndex().map { (iRow, row) ->
+                row.withIndex().map {
                         val coordinate = Pair(iRow, it.index)
                         coordinatesValues.getOrDefault(coordinate, data[coordinate])
                     }
-            }
-    )
+            })
 
     fun getCoordinatesWithValue(value: T): Sequence<IntCoordinate> = sequence {
         for ((iRow, row) in data.withIndex()) {
@@ -129,14 +115,18 @@ open class DataMap<T>(val data: List<List<T>>) {
         }
     }
 
-    protected fun IntCoordinate.isWithinBoundaries() =
-        (this.first in rowIndices && this.second in colIndices)
+    protected fun IntCoordinate.isWithinBoundaries() = (this.first in rowIndices && this.second in colIndices)
 
-    protected fun IntCoordinate.getNeighbours(predicate: (IntCoordinate) -> Boolean = { true }) =
-        Direction.entries
-            .map { this@getNeighbours + it }
-            .filter { it.isWithinBoundaries() }
+    protected fun IntCoordinate.getFourNeighbours(predicate: (IntCoordinate) -> Boolean = { true }) =
+        Direction.entries.map { this@getFourNeighbours + it }.filter { it.isWithinBoundaries() }
             .filter { predicate(it) }
+
+    protected fun IntCoordinate.getEightNeighbours(predicate: (IntCoordinate) -> Boolean = { true }) =
+        listOf(-1, 0, 1).flatMap { xOffset ->
+                listOf(-1, 0, 1).mapNotNull { yOffset ->
+                        if (xOffset == 0 && yOffset == 0) null else this + IntCoordinate(xOffset, yOffset)
+                    }
+            }.filter { it.isWithinBoundaries() }.filter { predicate(it) }
 
     fun floodMap(start: IntCoordinate, predicate: (T) -> Boolean): Set<IntCoordinate> {
         val expectedValue = data[start]
@@ -146,10 +136,7 @@ open class DataMap<T>(val data: List<List<T>>) {
         while (toExplore.isNotEmpty()) {
             val currentCoordinate = toExplore.pop() ?: break
             seen.add(currentCoordinate)
-            toExplore.addAll(
-                currentCoordinate
-                    .getNeighbours { this[it] == expectedValue }
-                    .filterNot { it in seen })
+            toExplore.addAll(currentCoordinate.getFourNeighbours { this[it] == expectedValue }.filterNot { it in seen })
         }
 
         return seen
